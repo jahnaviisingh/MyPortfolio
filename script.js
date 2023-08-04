172565003bc4986c7507536781326a13a30e61b8
Jahnavi Singh <54699354+jahnaviisingh@users.noreply.github.com>
2023-08-04 22:52:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const ham = document.getElementById("hamburger");
+
+// ham.addEventListener("click", () =>{
+//     console.log("heelo");
+// })
+
+console.log("hiii");
+console.log(ham);
+
+
+const light = document.getElementById("light-mode");
+
+light.addEventListener("click", () =>{
+    document.body.classList.toggle("dark-mode");
+})
+
+const nav = document.getElementById("go-pink");
+
+nav.addEventListener("click", () =>{
+    console.log("pinkkk");
+})
+
